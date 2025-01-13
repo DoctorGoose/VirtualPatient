5068f267a21e5dc9afd5965f97b259f15708ebe2
Doctor Goose <jordan.john.weaver@gmail.com>
2025-01-12 22:14:49 -0500

    Daily commit
    Figures cleanup mostly?

--- python/memory_model.jl
+++ python/memory_model.jl
@@ -47,22 +47,20 @@ function LCTModel!(du, u, p, t, state_history::StateHistory)
     z = u[7:end]
 
     # Unpack parameters
-    beta, k, p_param, c, delta, xi, a, d_E, delta_E, K_delta_E, zeta, eta, K_I1, tau_memory, d_TM = p
+    beta, k, p_param, c, delta, xi, a, d_E, delta_E, K_delta_E, zeta, eta, K_I1, tau_memory = p
 
     # Retrieve delayed state
-    CD8_E_tau = interpolate_delay(state_history, 4, t - tau_memory)  # 5th state (CD8_E)
+    CD8_E_tau = interpolate_delay(state_history, 5, t - tau_memory)  # 5th state (CD8_E)
 
-    # Conditionally set beta*T*V term to zero if V < 1?
-    beta_T_V = V < 1 ? 0.0 : beta * T * V
-    #beta_T_V = beta * T * V
-    eta_CD8M = CD8_M < 1.01 ? 0 : (eta * CD8_M * I1)/(I1 + K_I1)
+    #eta_CD8M = CD8_M < 1.01 ? 0 : (eta * CD8_M * I1)/(I1 + K_I1)
+    eta_CD8M = 0#xi * (CD8_M * I1)/(I1 + K_I1)
     # Equations
-    du[1] = -beta_T_V  # Target Cells
-    du[2] = beta_T_V - k * I1  # Eclipse Cells
+    du[1] = -beta * T * V  # Target Cells
+    du[2] = beta * T * V - k * I1  # Eclipse Cells
     du[3] = k * I1 - delta * I2 - delta_E * CD8_E * I2 / (K_delta_E + I2)  # Infected Cells
     du[4] = p_param * I2 - c * V  # Virus
-    du[5] = 0*a * z[end] + eta_CD8M - d_E * CD8_E  # Effector T Cells (+  a * z[end])
-    du[6] = 0#*zeta * CD8_E_tau - d_TM * CD8_M #- (eta * CD8_M * I1)/(I1 + K_I1)  # Memory T Cells
+    du[5] = a * z[end] + eta_CD8M - d_E * CD8_E  # Effector T Cells 
+    du[6] = zeta * CD8_E_tau # Memory T Cells
 
     # Delayed compartments
     du[7] = xi * I1 - a * z[1]
@@ -70,46 +68,19 @@ function LCTModel!(du, u, p, t, state_history::StateHistory)
 end
 
 
-function solve_reinfection(tspan, y0, params)
+function tmap_LCTModel(tspan, y0, param_sets)
+    param_sets = isa(param_sets, AbstractVector) && isa(param_sets[1], AbstractVector) ? param_sets : [param_sets]
+    sols = ThreadsX.map(params -> solve_LCTModel(tspan, y0, params), param_sets)
+    return length(sols) == 1 ? sols[1] : sols
+end
+
+function solve_LCTModel(tspan, y0, params)
     state_history = StateHistory(y0)
     wrapper!(du, u, p, t) = LCTModel!(du, u, p, t, state_history)
     prob = ODEProblem(wrapper!, y0, tspan, params)
-    
-    sol = solve(prob, TRBDF2(autodiff = false);
-                reltol = 1e-4, abstol = 1e-5,
-                dtmax = 1e-1, dtmin = 1e-8)
-    return (sol.t, hcat(sol.u...))
-end
 
-const THRESHOLD = 1.0  # Threshold for zero-crossing detection
-
-# Condition function for detecting near-zero crossing
-function condition(u, t, integrator)
-    # Check if any state is below the threshold
-    (u[2] < THRESHOLD) || (u[3] < THRESHOLD) || (u[4] < THRESHOLD)
-end
-
-# Affect function to modify state and derivative
-function affect!(integrator)
-    for i in 2:4  # Indices of I1, I2, V
-        if integrator.u[i] < THRESHOLD
-            integrator.u[i] = 0.0  # Set state to zero
-        end
-    end
-end
-
-# Create the callback
-cb = DiscreteCallback(condition, affect!)
-
-# Wrapper to solve the problem with the callback
-function solve_with_callback(tspan, y0, params)
-    state_history = StateHistory(y0)
-    wrapper!(du, u, p, t) = LCTModel!(du, u, p, t, state_history)
-    prob = ODEProblem(wrapper!, y0, tspan, params)
-    
-    sol = solve(prob, TRBDF2(autodiff = false);
-                #callback = cb,
-                reltol = 1e-4, abstol = 1e-5,
-                dtmax = 1e-1, dtmin = 1e-8)
+    isoutofdomain = (u, p, t) -> any(x -> x < -1e-3, u)
+    sol = suppress_warnings(() -> solve(prob, TRBDF2(autodiff=false); reltol=1e-4, abstol=1e-5, 
+                                         dtmax=1e-1, dtmin=1e-8, isoutofdomain=isoutofdomain))
     return (sol.t, hcat(sol.u...))
 end
